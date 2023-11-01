ef7c046090e72ab81187b6a7e9b2f8abb9d2c5a4
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-01 20:05:43 +0000

    add ProfileDeatil view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -9,3 +11,20 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+
+    def get(self, request, pk):
+        profile = get_object_or_404(Profile, pk=pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = get_object_or_404(Profile, pk=pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
