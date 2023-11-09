0a2ddf14fd7dbc11f575ed340ceab8725a1a0dbf
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-09 15:27:21 +0000

    add delete post view

--- posts/views.py
+++ posts/views.py
@@ -50,3 +50,8 @@ class PostDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
