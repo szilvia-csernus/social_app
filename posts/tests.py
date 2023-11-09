cfd7d9ae15babe94206a86ea73a657568601fa70
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-09 16:41:06 +0000

    add PostDetail test cases

--- posts/tests.py
+++ posts/tests.py
@@ -8,7 +8,6 @@ class PostListViewTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             username='szilvi', password='test_password')
-        self.client.login(username='szilvi', password='test_password')
 
     def test_can_list_posts(self):
         szilvi = User.objects.get(username='szilvi')
@@ -16,3 +15,66 @@ class PostListViewTests(APITestCase):
             owner=szilvi, title='Test post', content='Test content')
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='szilvi', password='test_password')
+        response = self.client.post(
+            '/posts/', {'title': 'Test post', 'content': 'Test content'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+
+    def test_not_logged_in_user_cannot_create_post(self):
+        response = self.client.post(
+            '/posts/', {'title': 'Test post', 'content': 'Test content'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        szilvi = User.objects.create_user(
+            username='szilvi', password='test_password')
+        User.objects.create_user(
+            username='another_user', password='test_password')
+        Post.objects.create(
+            owner=szilvi, title='Test post', content='Test content')
+
+    def test_can_retrieve_post(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_not_existing_post_returns_404(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_authenticated_user_can_update_post(self):
+        self.client.login(username='szilvi', password='test_password')
+        response = self.client.put(
+            '/posts/1/', {'title': 'Updated post',
+                          'content': 'Updated content'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        post = Post.objects.get(pk=1)
+        self.assertEqual(post.title, 'Updated post')
+
+    def test_not_authenticated_user_cannot_update_post(self):
+        self.client.login(username='another_user', password='test_password')
+        response = self.client.put(
+            '/posts/1/', {'title': 'Updated post',
+                          'content': 'Updated content'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authenticated_user_can_delete_post(self):
+        self.client.login(username='szilvi', password='test_password')
+        response = self.client.delete('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+
+    def test_not_authenticated_user_cannot_delete_post(self):
+        self.client.login(username='another_user', password='test_password')
+        response = self.client.delete('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from django.shortcuts import get_object_or_404
 from .models import Post
 from .serializers import PostSerializer
@@ -9,6 +9,7 @@ from social_app.permissions import IsOwnerOrReadOnly
 
 class PostList(APIView):
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         posts = Post.objects.all()
@@ -19,6 +20,7 @@ class PostList(APIView):
     def post(self, request):
         serializer = PostSerializer(data=request.data,
                                     context={'request': request})
+
         if serializer.is_valid():
             serializer.save(owner=request.user)
             return Response(serializer.data,
