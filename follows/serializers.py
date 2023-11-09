57e688ca47ad55ede56f0c485aba4c023df48761
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-09 22:10:25 +0000

    add serializer

--- follows/serializers.py
+++ follows/serializers.py
@@ -1,21 +1,22 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from .models import Follow
 
 
-class LikeSerializer(serializers.ModelSerializer):
+class FollowSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
-        model = Like
+        model = Follow
         fields = [
             'id',
             'owner',
+            'followed_name'
         ]
-        read_only_fields = ['owner']
+        read_only_fields = ['owner', 'followed_name']
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError('You already liked this post')
+            raise serializers.ValidationError('You already followed this person')
