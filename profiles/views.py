a365f8776c95cb3ef310f0a005682163a03c6154
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-10 08:22:22 +0000

    refactor Profile and Post views

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,53 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework import permissions, generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from social_app.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True,
-                                       context={'request': request})
-        return Response(serializer.data)
+# class ProfileList(APIView):
+#     def get(self, request):
+#         profiles = Profile.objects.all()
+#         serializer = ProfileSerializer(profiles, many=True,
+#                                        context={'request': request})
+#         return Response(serializer.data)
 
 
-class ProfileDetail(APIView):
+# class ProfileDetail(APIView):
+#     serializer_class = ProfileSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get_object(self, pk):
+#         profile = get_object_or_404(Profile, pk=pk)
+#         self.check_object_permissions(self.request, profile)
+#         return profile
+
+#     def get(self, request, pk):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(profile, context={'request': request})
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(profile,
+#                                        data=request.data,
+#                                        context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors,
+#                         status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        profile = get_object_or_404(Profile, pk=pk)
-        self.check_object_permissions(self.request, profile)
-        return profile
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile,
-                                       data=request.data,
-                                       context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
