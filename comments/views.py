b43f19083e4128471d31d216b852ac4cdf42a6af
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-12 09:50:52 +0000

    add filterset field to comments

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from social_app.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -17,6 +18,14 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        # filter the comments by post
+        'post',
+    ]
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
