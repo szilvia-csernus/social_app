62a28265d1e417a030c096e86d1e6b1449172240
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-13 09:31:29 +0000

    add pagination

--- social_app/settings.py
+++ social_app/settings.py
@@ -37,6 +37,9 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 REST_AUTH = {
